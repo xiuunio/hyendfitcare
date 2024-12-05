3de57bddd5428ccd58ad34fe14929aeb22f70f19
leesieun <leesieun030428@gmail.com>
2024-12-05 15:04:13 +0900

    hyend front all

---
+++ script.js
@@ -0,0 +1,19 @@
+const months = [
+    '1',
+    '2',
+    '3',
+    '4',
+    '5',
+    '6',
+    '7',
+    '8',
+    '9',
+    '10',
+    '11',
+    '12',
+  ];
+
+let date = new Date();
+let currYear = date.getFullYear(),
+  currMonth = date.getMonth();
+
